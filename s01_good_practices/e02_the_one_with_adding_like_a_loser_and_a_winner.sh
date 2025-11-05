d2778dd43ec1321fcd9c07481dafe86d86934608
Robert Sandberg <robert.sandberg@forefront.se>
2025-11-05 12:50:40 +0100

    remove windows style line ending

--- s01_good_practices/e02_the_one_with_adding_like_a_loser_and_a_winner.sh
+++ s01_good_practices/e02_the_one_with_adding_like_a_loser_and_a_winner.sh
@@ -27,8 +27,8 @@ git commit -qm "Initial commit"
 
 echo "+ Make changes to $file1"
 sed -b -i '2s/what/who/' $file1
-sed -b -i '16s/.*/Thanks you for your tiny car,\r/' $file1
-sed -b -i '17s/.*/He does not want to see you anymore,\r/' $file1
-sed -b -i '18s/.*/If you did not blinkat so.\r/' $file1
-sed -b -i '20s/.*/Yet another change.\r/' $file1
+sed -b -i '16s/.*/Thanks you for your tiny car,/' $file1
+sed -b -i '17s/.*/He does not want to see you anymore,/' $file1
+sed -b -i '18s/.*/If you did not blinkat so./' $file1
+sed -b -i '20s/.*/Yet another change./' $file1
 cp $filepath/$file2 .
--- s01_good_practices/e03_the_one_with_rebasing_feature_branches.sh
+++ s01_good_practices/e03_the_one_with_rebasing_feature_branches.sh
@@ -28,7 +28,7 @@ git commit -qam "Add lyrics"
 echo "+ Add a few commits on main"
 for i in {1..5}
 do
-  echo "add $i line\r" >> ./$file1
+  echo "add $i line" >> ./$file1
   git commit -qam "add change $i"
 done
 
@@ -39,19 +39,19 @@ git checkout -qb feature2
 cp $filepath/$file2 .
 git add $file2
 git commit -qm "f2: add swedish twinkle"
-echo "National anthem material!\r" >> $file2
+echo "National anthem material!" >> $file2
 git add $file2
 git commit -qm "f2: add opinion"
-echo "Really awesome song!\r" >> $file2
+echo "Really awesome song!" >> $file2
 git add $file2
 git commit -qm "f2: add second opinion"
 
 git checkout -q feature1
 touch $file3
-echo "This repo is awesome\r" >> $file3
+echo "This repo is awesome" >> $file3
 git add $file3
 git commit -qm "f1: add readme"
-echo "This repo is really awesome\r" >> $file3
+echo "This repo is really awesome" >> $file3
 git add $file3
 git commit -qm "f1: improve readme"
 
@@ -61,6 +61,6 @@ echo "+ Add a few more commits on main"
 echo
 for i in {6..10}
 do
-  echo "add $i line\r" >> ./$file1
+  echo "add $i line" >> ./$file1
   git commit -qam "add change $i"
 done
